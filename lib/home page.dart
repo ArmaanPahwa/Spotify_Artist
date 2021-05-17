445835f7ffbdbe802080de77d56363b032e93364
ArmaanPahwa <60634104+ArmaanPahwa@users.noreply.github.com>
2021-05-17 15:40:00 -0400

    Search Menu frame creation

--- lib/home page.dart
+++ lib/home page.dart
@@ -22,7 +22,7 @@ class _SpotifyArtistHomePage extends State<SpotifyArtistHomePage> {
           IconButton(
               icon: Icon(Icons.search),
               onPressed: () {
-                showSearch(context: context, delegate: null);
+                showSearch(context: context, delegate: DataSearch());
               }),
         ],
       ),
@@ -30,6 +30,63 @@ class _SpotifyArtistHomePage extends State<SpotifyArtistHomePage> {
         child: Text('Please use the search icon to begin your artist search',
             style: TextStyle(fontSize: 16.0, color: Colors.white)),
       ),
+    );
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+  var entries = [
+    "Entry 1",
+    "Entry 2",
+    "Entry 3",
+    "Entry 4",
+    "Entry 5",
+  ];
+
+  var suggestedEntries = [
+    "Entry 1",
+    "Entry 2",
+  ];
+
+  //Creates the clear search query
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
+    ];
+  }
+
+  //Creates back button from search
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        icon: AnimatedIcon(
+            icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty ? suggestedEntries : entries;
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.music_note),
+        title: Text(suggestionList[index]),
+      ),
+      itemCount: suggestionList.length,
     );
   }
 }
