54e775b203cd41d4d16238848845a1c0775d4d1e
ArmaanPahwa <60634104+ArmaanPahwa@users.noreply.github.com>
2021-05-17 15:37:12 -0400

    Home page UI polish

--- lib/home page.dart
+++ lib/home page.dart
@@ -22,36 +22,14 @@ class _SpotifyArtistHomePage extends State<SpotifyArtistHomePage> {
           IconButton(
               icon: Icon(Icons.search),
               onPressed: () {
-                showSearch(context: context, delegate: DataSearch());
-              })
+                showSearch(context: context, delegate: null);
+              }),
         ],
       ),
+      body: Center(
+        child: Text('Please use the search icon to begin your artist search',
+            style: TextStyle(fontSize: 16.0, color: Colors.white)),
+      ),
     );
-  }
-}
-
-class DataSearch extends SearchDelegate<String> {
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    // TODO: implement buildActions
-    throw UnimplementedError();
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
-    throw UnimplementedError();
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
-    throw UnimplementedError();
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    throw UnimplementedError();
   }
 }
