14921bcdf0da0f42cbeee667c1614ab2df2f0ef7
ArmaanPahwa <60634104+ArmaanPahwa@users.noreply.github.com>
2021-05-24 23:32:28 -0400

    Artist Class Factory and Structure Completion

--- lib/data-models/artist.dart
+++ lib/data-models/artist.dart
@@ -1,14 +1,26 @@
 class Artist {
   String name;
+  int followers;
+  String imageUrl;
 
-  Artist(this.name);
+  Artist(this.name, this.followers, this.imageUrl);
 
   factory Artist.fromJson(Map json) {
-    //Frame for artist factory. To be edited later.
-    return Artist(json['name']);
+    String url = json['images'].isEmpty
+        ? 'https://picsum.photos/id/119/200/200'
+        : json['images'][0]['url'];
+    return Artist(json['name'], json['followers']['total'], url);
   }
 
   String getArtistName() {
     return this.name;
+  }
+
+  int getArtistFollowers() {
+    return this.followers;
+  }
+
+  String getArtistImageUrl() {
+    return this.imageUrl;
   }
 }
