e2428be8acebd59c61b0943cae75c2de75b06f32
ArmaanPahwa <60634104+ArmaanPahwa@users.noreply.github.com>
2021-05-25 19:05:03 -0400

    Artist Image and Name displayed

--- lib/artist page.dart
+++ lib/artist page.dart
@@ -13,15 +13,43 @@ class ArtistPage extends StatefulWidget {
 class _ArtistPageState extends State<ArtistPage> {
   @override
   Widget build(BuildContext buildContext) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Color(0xFF1DB954),
           brightness: Brightness.dark,
           centerTitle: true,
           title: Text('Artist Information')),
-      body: Center(
-        child: Text('You have reached: ${widget.artist.name} \'s page'),
-      ),
+      body: getBody(widget.artist, size),
     );
   }
+}
+
+Widget getBody(Artist artist, var size) {
+  /*return Center(
+    child: Text('You have reached: ${artist.name} \'s page'),
+  );*/
+  return Center(
+    child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 22.0),
+      child: Column(
+        children: [
+          Container(
+              width: size.width / 1.5,
+              height: size.width / 1.5,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(300),
+                child: Image.network(artist.imageUrl),
+              )),
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text(
+              "${artist.name}",
+              style: TextStyle(fontSize: 28),
+            ),
+          )
+        ],
+      ),
+    ),
+  );
 }
